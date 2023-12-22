18fb2622a35f584912e57b0a785c13214c18bddc
Gdk237 <146049531+Lsh2375000@users.noreply.github.com>
2023-12-22 11:57:32 +0900

    커리어넷 오픈API적용 멘토 평점 정보 출력

---
+++ src/main/java/kr/nomadlab/mentors/main/service/MentorReviewServiceImpl.java
@@ -0,0 +1,20 @@
+package kr.nomadlab.mentors.main.service;
+
+import kr.nomadlab.mentors.main.dto.MentorReviewDTO;
+import kr.nomadlab.mentors.main.mapper.MentorReviewMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class MentorReviewServiceImpl implements MentorReviewService{
+
+    private final MentorReviewMapper mentorReviewMapper;
+
+    @Override
+    public int mentorReviewCount(Long mno) {
+        return mentorReviewMapper.mentorReviewCount(mno);
+    }
+}
