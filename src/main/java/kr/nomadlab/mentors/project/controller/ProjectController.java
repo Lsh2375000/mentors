7f2061869140e42bc14bb3b683f2594bfe7defc0
LeeHeonGu <gjsrn2006@naver.com>
2023-12-27 21:19:18 +0900

    게시판 회원 인증 권한 조건 부여

--- src/main/java/kr/nomadlab/mentors/project/controller/ProjectController.java
+++ src/main/java/kr/nomadlab/mentors/project/controller/ProjectController.java
@@ -7,11 +7,13 @@ import kr.nomadlab.mentors.board.dto.HashTagDTO;
 import kr.nomadlab.mentors.board.service.BoardService;
 import kr.nomadlab.mentors.common.PageRequestDTO;
 import kr.nomadlab.mentors.common.PageResponseDTO;
+import kr.nomadlab.mentors.member.dto.MemberSecurityDTO;
 import kr.nomadlab.mentors.project.dto.ProjectDTO;
 import kr.nomadlab.mentors.project.dto.ProjectTagDTO;
 import kr.nomadlab.mentors.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -60,7 +62,9 @@ public class ProjectController {
     }
 
     @PostMapping("/register")
-    public String registerPOST(ProjectDTO projectDTO, @RequestParam("hashTags") String[] hashTags){ // 게시글 등록
+    public String registerPOST(ProjectDTO projectDTO,
+                               @RequestParam("hashTags") String[] hashTags,
+                               @AuthenticationPrincipal MemberSecurityDTO member){ // 게시글 등록
         log.info("/project/register(POST)...");
         log.info(projectDTO);
 
@@ -75,7 +79,7 @@ public class ProjectController {
             log.info("No Hash Tags provided.");
         }
 
-        projectDTO.setMno(1L);
+        projectDTO.setMno(member.getMno());
         
         projectService.registerProject(projectDTO);
 
