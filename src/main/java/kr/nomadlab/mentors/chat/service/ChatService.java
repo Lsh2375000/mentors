d309d4512fbda247e99b03256775fee2e3794b29
LeeHeonGu <gjsrn2006@naver.com>
2023-12-28 03:51:24 +0900

    코인 환불, 마이페이지 링크 추가

--- src/main/java/kr/nomadlab/mentors/chat/service/ChatService.java
+++ src/main/java/kr/nomadlab/mentors/chat/service/ChatService.java
@@ -4,6 +4,7 @@ import kr.nomadlab.mentors.chat.dto.ChatListDTO;
 import kr.nomadlab.mentors.chat.dto.ChatMessageDTO;
 import kr.nomadlab.mentors.chat.dto.ChatRoomDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ChatService {
@@ -15,5 +16,5 @@ public interface ChatService {
     public ChatRoomDTO getRoom(String roomId); // 채팅방 아이디와 일치하는 채팅방 조회
     public List<ChatListDTO> getChatMembers(String roomId); // 채팅방에 참여중인 회원 목록 조회
     public Boolean findMemberInRoom(String roomId, Long mno); // 채팅방에 해당 회원 존재 여부
-    public void removeChatMember(String roomid, Long mno, Long mbNo); // 채팅방에 나간 회원 정보 삭제
+    public void removeChatMember(String roomid, Long mno, Long mbNo, LocalDate startDate); // 채팅방에 나간 회원 정보 삭제
 }
